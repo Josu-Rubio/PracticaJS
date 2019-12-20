c13ad6c5aa5f3a8a20eb35850c3c784cc5e087b2
Josué Rubio <josu_mr@hotmail.com>
2019-12-20 23:50:37 +0100

    Empezamos ejercicio 2

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,27 +1,16 @@
-const changeArabToRom = digit => {
-    if (digit < 1 || digit > 3999) {
-        return 'Not a valid number'
-    } else {
-        let resultRoman = '';
-        const romanDictionary = {
-            I: '1',
-            V: '5',
-            X: '10',
-            L: '50',
-            C: '100',
-            D: '500',
-            M: '1000'
-        };
-
-
-        let i;
-        for (i in romanDictionary) {
-            while (i >= romanDictionary[i]) {
-                resultRoman += i;
-                digit -= romanDictionary[i];
-            }
-        }
-        return resultRoman;
-    }
-}
-console.log(changeArabToRom('1199'))
+let changeArabToRoman = digit => {
+    let romanDictionary = [
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
+        [10, 'X'],
+        [9, 'IX'],
+        [5, 'V'],
+        [4, 'IV'],
+        [1, 'I']
+    ];
