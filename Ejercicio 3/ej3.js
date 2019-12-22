dc3c338f5045a0e0be1e2b3a4c9ada6cc13ac2b0
Josué Rubio <josu_mr@hotmail.com>
2019-12-22 22:21:51 +0100

    Stuck in the Ex3

--- Ejercicio 3/ej3.js
+++ Ejercicio 3/ej3.js
@@ -4,7 +4,14 @@ let player2 = [];
 
 const points = {
     highCard: 0,
+    pair: 1,
+    twoPair: 2,
+    threeOfAKind: 3,
+    straight: 4,
+    flush: 5,
+    fullHouse: 6,
     poker: 7,
+    straightFlush: 8,
 }
 
 const addCard = (value, suit) => {
@@ -16,7 +23,13 @@ const addCard = (value, suit) => {
         case 11:
             val = 'J'
             break;
-        case 1:
+        case 12:
+            val = 'Q'
+            break;
+        case 13:
+            val = 'K'
+            break;
+        case 14:
             val = 'A'
             break;
         default:
@@ -27,24 +40,42 @@ const addCard = (value, suit) => {
         value: val,
         suit
     });
+
 }
 
 const removeRandomCard = () => {
     const rand = (Math.random(new Date().getMilliseconds()) * 100) % baraja.length;
     const card = baraja.splice(rand, 1);
-    return card;
+    return card[0];
 }
 
-for (let i = 1; i <= 13; i++) {
+
+for (let i = 2; i <= 14; i++) {
     addCard(i, 'S');
     addCard(i, 'H');
+    addCard(i, 'C');
+    addCard(i, 'D');
 }
+
 
 for (let i = 0; i < 5; i++) {
     player1.push(removeRandomCard());
     player2.push(removeRandomCard());
 }
 
-console.log('baraja', baraja);
-console.log('player 1', player1);
-console.log('player 2', player2)
+let handPlayer1 = player1.map(obj => {
+    let rObj = {};
+    rObj[obj.value] = obj.suit;
+    return rObj;
+});
+let handPlayer2 = player2.map(obj => {
+    let rObj = {};
+    rObj[obj.value] = obj.suit;
+    return rObj;
+});
+
+
+
+//console.log('baraja', baraja);
+console.log('player 1', handPlayer1);
+console.log('player 2', handPlayer2)
